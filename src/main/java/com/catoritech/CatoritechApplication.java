cad7d8a5fbda011e4d27f008cf476f96e8c629fa
BojanVr <bojanvrskovski@gmail.com>
2023-09-15 12:46:25 +0200

    starting the project

---
+++ src/main/java/com/catoritech/CatoritechApplication.java
@@ -0,0 +1,13 @@
+package com.catoritech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatoritechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatoritechApplication.class, args);
+	}
+
+}
