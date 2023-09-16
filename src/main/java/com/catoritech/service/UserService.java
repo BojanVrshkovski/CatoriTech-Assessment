397ed1ef9955dc9e633b2bb573f6e6d735b2eba4
BojanVr <bojanvrskovski@gmail.com>
2023-09-16 12:09:19 +0200

    added the service classes

---
+++ src/main/java/com/catoritech/service/UserService.java
@@ -0,0 +1,5 @@
+package com.catoritech.service;
+
+public interface UserService {
+
+}
---
+++ src/main/java/com/catoritech/service/impl/UserServiceImpl.java
@@ -0,0 +1,9 @@
+package com.catoritech.service.impl;
+
+import com.catoritech.service.UserService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+}
