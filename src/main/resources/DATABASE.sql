eab5e966b1dd9645f90eee71899892b129b18c24
BojanVr <bojanvrskovski@gmail.com>
2023-09-16 11:48:08 +0200

    added the database connection and the db file

---
+++ src/main/resources/DATABASE.sql
@@ -0,0 +1,17 @@
+CREATE TABLE contact_details(
+	id BIGSERIAL PRIMARY KEY,
+	username varchar UNIQUE NOT NULL,
+	password varchar NOT NULL,
+	contact_role varchar NOT NULL
+);
+
+CREATE TABLE contacts(
+	id BIGSERIAL PRIMARY KEY,
+	first_name VARCHAR(100) NOT NULL,
+	last_name VARCHAR(100) NOT NULL,
+	address VARCHAR(100) NOT NULL,
+	phone VARCHAR(100) NOT NULL UNIQUE,
+	VAT VARCHAR(20) NOT NULL,
+	user_id BIGINT,
+    FOREIGN KEY (user_id) REFERENCES contact_details(id) ON DELETE CASCADE
+);
