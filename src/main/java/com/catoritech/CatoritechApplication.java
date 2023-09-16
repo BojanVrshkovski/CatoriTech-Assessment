a90df6d732db25f79b6f75a329d07d79d4021fc7
BojanVr <bojanvrskovski@gmail.com>
2023-09-16 14:18:37 +0200

    added the security config

--- src/main/java/com/catoritech/CatoritechApplication.java
+++ src/main/java/com/catoritech/CatoritechApplication.java
@@ -2,6 +2,9 @@ package com.catoritech;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class CatoritechApplication {
@@ -10,4 +13,8 @@ public class CatoritechApplication {
 		SpringApplication.run(CatoritechApplication.class, args);
 	}
 
+	@Bean
+	PasswordEncoder passwordEncoder(){
+		return new BCryptPasswordEncoder(10);
+	}
 }
