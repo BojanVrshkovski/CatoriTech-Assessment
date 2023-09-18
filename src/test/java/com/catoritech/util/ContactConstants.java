6df44dfd946fdb686e062186765d3c934d4bf0ec
BojanVr <bojanvrskovski@gmail.com>
2023-09-18 12:49:04 +0200

    added the ContactConstants class

---
+++ src/test/java/com/catoritech/util/ContactConstants.java
@@ -0,0 +1,11 @@
+package com.catoritech.util;
+
+public class ContactConstants {
+	public static final Long ID = 1L;
+	public static final String FIRST_NAME = "Bojan";
+	public static final String LAST_NAME = "Vrshkovski";
+	public static final String ADDRESS = "Address";
+	public static final String PHONE = "077777123";
+	public static final String VAT = "VAT";
+	public static final Long USER_ID = 2L;
+}
