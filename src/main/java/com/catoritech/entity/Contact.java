b1ee92e243c3cd2ad180fe5f70162a4593ba2c26
BojanVr <bojanvrskovski@gmail.com>
2023-09-18 11:43:06 +0200

    added the contact entity

---
+++ src/main/java/com/catoritech/entity/Contact.java
@@ -0,0 +1,107 @@
+package com.catoritech.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "contacts")
+public class Contact {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(name = "first_name", nullable = false)
+	private String firstName;
+
+	@Column(name = "last_name", nullable = false)
+	private String lastName;
+
+	@Column(name = "address", nullable = false)
+	private String address;
+
+	@Column(name = "phone", nullable = false, unique = true)
+	private String phone;
+
+	@Column(name = "VAT", nullable = false)
+	private String VAT;
+
+	@JoinColumn(name = "user_id", referencedColumnName = "id")
+	private Long userId;
+
+	public Contact() {
+	}
+
+	public Contact(Long id, String firstName, String lastName, String address, String phone, String VAT, Long userId) {
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.address = address;
+		this.phone = phone;
+		this.VAT = VAT;
+		this.userId = userId;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getVAT() {
+		return VAT;
+	}
+
+	public void setVAT(String VAT) {
+		this.VAT = VAT;
+	}
+
+	public Long getUserId() {
+		return userId;
+	}
+
+	public void setUserId(Long userId) {
+		this.userId = userId;
+	}
+}
