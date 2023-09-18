ee94f56ee790bbcc824f34e4bfd455d8f52b4ee7
BojanVr <bojanvrskovski@gmail.com>
2023-09-18 12:01:42 +0200

    added the controlle class and the create method

---
+++ src/main/java/com/catoritech/controller/ContactController.java
@@ -0,0 +1,38 @@
+package com.catoritech.controller;
+
+import com.catoritech.entity.requests.ContactRequest;
+import com.catoritech.entity.requests.UserRequest;
+import com.catoritech.service.ContactService;
+import com.catoritech.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+
+@RestController
+public class ContactController {
+	private final ContactService contactService;
+
+	@Autowired
+	public ContactController(ContactService contactService) {
+		this.contactService = contactService;
+	}
+
+	@PostMapping("/createContact")
+	@PreAuthorize("hasAuthority('BUSINESS')")
+	public ResponseEntity<Void> createContact(@RequestBody ContactRequest contactRequest) {
+		Long id = contactService.createContact(contactRequest);
+
+		URI location = UriComponentsBuilder.fromUriString("/contact/{id}")
+		                                   .buildAndExpand(id)
+		                                   .toUri();
+
+		return ResponseEntity.created(location).build();
+	}
+
+}
